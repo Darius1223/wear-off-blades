aa21e44d4886ac2d0f4c47a6b7c6219a95a7d122
Darius1223 <dar.290199@mail.ru>
2022-05-26 20:44:22 +0700

    EDIT: correct plot+ table functional

--- entites.py
+++ entites.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Tuple, List
 
 import settings
@@ -5,22 +6,39 @@ import settings
 
 class PlotData:
     def __init__(self):
-        self.current_row_index: int = 0
+        self.current_plot_index: int = 0
         self.row_count: int = settings.DEFAULT_ROW_COUNT
-        self.data: List[List[Tuple[int, int]]] = [settings.DEFAULT_DATA]
+        self.plot_count: int = settings.DEFAULT_PLOT_COUNT
+        self.data: List[List[List[int, int]]] = [settings.DEFAULT_DATA]
+
+    def fill_rows(self, new_count: int) -> None:
+        dist = new_count - self.row_count
+        for ind_plot, _ in enumerate(self.data):
+            for _ in range(abs(dist)):
+                if dist > 0:
+                    self.data[ind_plot].append(copy.copy(settings.DEFAULT_VECTOR))
+                else:
+                    self.data[ind_plot].pop()
+
+    def fill_plot(self, new_count: int) -> None:
+        dist = new_count - self.plot_count
+        for _ in range(abs(dist)):
+            if dist > 0:
+                self.data.append(
+                    [copy.copy(settings.DEFAULT_VECTOR) for _ in range(self.row_count)])
+            else:
+                self.data.pop()
 
     @property
     def current_plot(self):
-        return self.data[self.current_row_index]
+        return self.data[self.current_plot_index]
 
     @current_plot.setter
     def current_plot(self, value):
-        self.data[self.current_row_index] = list(value)
+        self.data[self.current_plot_index] = list(value)
 
-    @property
-    def x_data(self):
-        return [vector[0] for vector in self.current_plot]
+    def x_data(self, ind):
+        return [vector[0] for vector in self.data[ind]]
 
-    @property
-    def y_data(self):
-        return [vector[1] for vector in self.current_plot]
+    def y_data(self, ind):
+        return [vector[1] for vector in self.data[ind]]
